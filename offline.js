7c58f45856c64466b5ca28ae75ee710d9587c6ca
KokokAyam <75647574+KokokAyam@users.noreply.github.com>
2021-04-08 13:43:16 +0700

    initial commit

---
+++ offline.js
@@ -0,0 +1,18 @@
+﻿{
+	"version": 1617864141,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/butawarna-sheet0.png",
+		"images/butawarna-sheet1.png",
+		"images/restart-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
